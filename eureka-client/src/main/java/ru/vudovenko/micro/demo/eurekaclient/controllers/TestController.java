d55fa89ccc5cc060293614760fb644e95092b153
vudovenko <vladislav.udovenko0@yandex.ru>
2024-08-16 03:09:54 +0500

    тестирование балансировщика нагрузки из API Gateway

--- eureka-client/src/main/java/ru/vudovenko/micro/demo/eurekaclient/controllers/TestController.java
+++ eureka-client/src/main/java/ru/vudovenko/micro/demo/eurekaclient/controllers/TestController.java
@@ -1,5 +1,6 @@
 package ru.vudovenko.micro.demo.eurekaclient.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/main")
 public class TestController {
 
+    @Value("${eureka.instance.instance-id}")
+    private String id;
+
     @GetMapping("/test")
     public String test() {
-        return "test1";
+        return id;
     }
 }
