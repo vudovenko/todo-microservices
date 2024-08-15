afb4c6fa4976afd4ab2bda849026cb85d1603ea5
vudovenko <vladislav.udovenko0@yandex.ru>
2024-08-16 01:38:39 +0500

    создал приложение eclient2

---
+++ eureka-client-2/src/test/java/ru/vudovenko/micro/demo/eurekaclient2/EurekaClient2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vudovenko.micro.demo.eurekaclient2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClient2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
