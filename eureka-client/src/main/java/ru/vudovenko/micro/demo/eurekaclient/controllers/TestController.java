a498a01190e4562735ce2386cdd0924c95abc75e
vudovenko <vladislav.udovenko0@yandex.ru>
2024-08-16 21:03:07 +0500

    считывание конфигов с удаленного репозитория для eclient

--- eureka-client/src/main/java/ru/vudovenko/micro/demo/eurekaclient/controllers/TestController.java
+++ eureka-client/src/main/java/ru/vudovenko/micro/demo/eurekaclient/controllers/TestController.java
@@ -12,8 +12,11 @@ public class TestController {
     @Value("${eureka.instance.instance-id}")
     private String id;
 
+    @Value("${tmpVar}")
+    private int tmpVar;
+
     @GetMapping("/test")
     public String test() {
-        return id;
+        return id + ":" + tmpVar;
     }
 }
